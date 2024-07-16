9ba28379deeefeb2d1b5e73677983fb5dbfdf37b
dronus <paul.geisler@web.de>
2024-07-16 22:03:29 +0200

    add OTA updater

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <LittleFS.h>
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
+#include <ArduinoOTA.h>
 #include <ArtnetnodeWifi.h>
 #include "wifi_config.h" // provides wifiName, wifiSecret
 #include "FastAccelStepper.h"
@@ -59,6 +60,37 @@ void setup()
   Serial.println();
   Serial.print("connected, IP address: ");
   Serial.println(WiFi.localIP().toString().c_str());
+
+  ArduinoOTA
+    .onStart([]() {
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        Serial.println("Start updating sketch");
+      } else {  // U_SPIFFS
+        Serial.println("Start updating files");
+      }
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
 
   httpServer = new AsyncWebServer(80);
 
@@ -171,4 +203,6 @@ void loop()
     homing = 0;
     dmxChannel = prefs.getInt("channel",0);
   }
+
+  ArduinoOTA.handle();
 }
