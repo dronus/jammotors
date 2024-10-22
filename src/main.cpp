e15a90a5371d34979cca7794adfa6e103b1a42a4
dronus <paul.geisler@web.de>
2024-10-22 15:27:25 +0200

    prepare for multiple channels - store parameters by channel, using param.h

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "driver/twai.h"
 #include "xiaomi_cybergear_driver.h"
 #include <Preferences.h>
-
+#include "param.h"
 
 #define statusLedPin 2
 
@@ -23,77 +23,83 @@ ArtnetnodeWifi artnetnode;
 AsyncWebServer *httpServer;
 DNSServer dnsServer;
 
-bool dmx_enabled = true;
-bool have_alarm=0;
 int homing = 0;
-int enabled=0;
-int32_t target = 0;
 
-// type for a settable, storable parameter.
-// May hold an "apply" method that should be called after changing the parameter.
-// Provides "trySet" to check a webserver request for changes in this parameter.
-struct Param {
-  enum ParamType {INT, STRING} type;
-  const char* name;
-  void (*apply)(Param&) = NULL;
-
-  Param(ParamType _type, const char* _name, void(*_apply)(Param&) = NULL) : type(_type), name(_name),apply(_apply) {};
-
-  void trySet(AsyncWebServerRequest* request) {
-    if(! request->hasParam(name) ) return;
-    if(type == INT)
-      prefs.putInt(name, request->getParam(name)->value().toInt());
-    else if(type == STRING)
-      prefs.putString(name, request->getParam(name)->value());
-    if(apply != NULL)
-      (*apply)(*this);
-  }
-};
-
-struct Channel {
+struct Channel : public Params {
   XiaomiCyberGearDriver cybergear;
-  Channel(int _can_id) : cybergear(_can_id,MASTER_CAN_ID) {}
-  void init_motor() {
-    // initialize CyberGear on CAN bus
+  Channel(int _can_id) : cybergear(_can_id,MASTER_CAN_ID), can_id(_can_id){}
+
+  P_int32_t (poweron_enable, 0, 1, 0);
+  P_int32_t (speed, 0, 100000, 10000);
+  P_int32_t (position_kp, 0, 10000, 1000);
+  P_int32_t (accel, 0, 100000, 10000);
+  P_int32_t (channel, 0, 255, 0);
+  P_int32_t (scale, 0, 10000,  0);
+  P_int32_t (osc_f, 0, 10000,  0);
+  P_int32_t (osc_a, 0, 100000, 0);
+  P_int32_t (random_d, 0, 100000, 1000);
+  P_int32_t (random_rd,0, 100000, 1000);
+  P_int32_t (random_a ,0, 10000, 0);
+
+  int can_id;
+  int enabled;
+  int have_alarm;
+  int32_t target = 0;
+  int32_t manual_target=0;
+  int32_t artnet_target=0;
+  float osc_phase = 0;
+  int32_t random_countdown = 0;
+  int32_t random_target = 0;
+
+  void init() {
+    // initialize CyberGear on CAN bus    
     cybergear.init_twai(RX_PIN, TX_PIN, /*serial_debug=*/true);
     cybergear.init_motor(MODE_POSITION);
-    cybergear.set_position_kp(prefs.getInt("position_kp",1000)/1000.f);
+    cybergear.set_position_kp(position_kp/1000.f);
     cybergear.enable_motor(); /* turn on the motor */
     cybergear.set_position_ref(0.0); /* set initial rotor position */
+    enabled = poweron_enable;
     Serial.println("Cybergear started.");
   }
-  void update() {
-    cybergear.set_limit_speed(prefs.getInt("speed",10000)/1000.f);
-    cybergear.set_position_kp(prefs.getInt("position_kp",1000)/1000.f);
-    cybergear.set_limit_current(enabled * prefs.getInt("accel",10000)/1000.f);
-    // TODO cybergear.set_position_ref(0.0); 
+  void update(uint32_t dt) {
+    cybergear.set_limit_speed(speed/1000.f);
+    cybergear.set_position_kp(position_kp/1000.f);
+    cybergear.set_limit_current(enabled * accel/1000.f);
+
+    // compute and set motion target
+    // set manual target (offset)
+    target = manual_target;
+
+    // add artnet commanded target
+    target += artnet_target;
+   
+    // add oscillatory movement 
+    osc_phase += osc_f * dt * 2.f * 3.14159f / 1000.f / 1000.f;
+    osc_phase = fmod(osc_phase, ((float)PI * 2.f));
+    target += floor( osc_a * sin(osc_phase) );
+    
+    // add random movement
+    random_countdown -= dt;
+    if(random_countdown < 0) {
+      random_target    = random(random_a);
+      random_countdown = random_d + random(random_rd);
+    }
+    target += random_target;
+
+    // execute move
+    cybergear.set_position_ref(target/1000.f);
   }
 };
 
-Channel channels[] = {
+const uint8_t max_channels = 1;
+Channel channels[max_channels] = {
   Channel(CYBERGEAR_CAN_ID)
 };
 
-// list of settable and stored parameters
-Param params[] = {
-  {Param::INT, "poweron_enable" },
-  {Param::INT, "speed"},
-  {Param::INT, "position_kp"},
-  {Param::INT, "accel"},
-  {Param::INT, "channel" },
-  {Param::INT, "scale" },
-  {Param::INT, "osc_f" },
-  {Param::INT, "osc_a" },
-  {Param::INT, "random_d" },
-  {Param::INT, "random_rd" },
-  {Param::INT, "random_a" }
-  ,
-//};
-
-//Param global_params[] = {
-  {Param::STRING, "name" },
-  {Param::STRING, "ssid" },
-  {Param::STRING, "psk" }
+char* global_params[] = {
+  "name",
+  "ssid",
+  "psk"
 };
 
 // switch WiFi to acces point mode and provide an captive portal page.
@@ -112,14 +118,12 @@ void switchWifiAp() {
   dnsServer.start(53, "*", WiFi.softAPIP());
 }
 
-// intermediate position targets set by manual input or incoming artnet messages
-uint32_t manual_target=0;
-uint32_t artnet_target=0;
-
 // receive incoming CAN messages from CyberGear motor and forward them to driver
 static void handle_rx_message(twai_message_t& message) {
-  if (((message.identifier & 0xFF00) >> 8) == CYBERGEAR_CAN_ID){
-    channels[0].cybergear.process_message(message);
+  for(Channel& channel : channels) {
+    if (((message.identifier & 0xFF00) >> 8) == channel.can_id){
+      channel.cybergear.process_message(message);
+    }
   }
 }
 
@@ -133,8 +137,8 @@ static void check_alerts(){
 
   // Handle alerts
   if (alerts_triggered & TWAI_ALERT_ERR_PASS) {
-    have_alarm = true;
-    enabled = false;
+    channels[0].have_alarm = true;
+    channels[0].enabled = false;
     Serial.println("Alert: TWAI controller has become error passive.");
   }
   if (alerts_triggered & TWAI_ALERT_BUS_ERROR) {
@@ -166,10 +170,19 @@ void setup()
   // status LED, start off and turn on once initalisation is complete.
   pinMode(statusLedPin,OUTPUT);
 
-  enabled = prefs.getInt("poweron_enable",0);
-
-  for(Channel& channel : channels) 
-    channel.init_motor();
+  for(uint8_t channel_id = 0; channel_id<max_channels; channel_id++) {
+    Channel& channel = channels[channel_id];
+    
+    // read preferences
+    Param* p = channel.getParams();
+    do {
+      char prefs_name[32];
+      snprintf(prefs_name, sizeof(prefs_name), "%s_%d", p->desc->name, channel_id);      
+      p->set(prefs.getInt(prefs_name, p->desc->def));
+    } while ( p = p->next() );
+    // initialize
+    channel.init();
+  }
 
 //  Serial.begin(115200);
   Serial.println("NF Motor - WiFi ArtNet stepper motor driver with web interface");
@@ -232,54 +245,61 @@ void setup()
   httpServer->on("/set", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(204);
 
-    // handle persistent preferences
-    for(Param& p : params)
-      p.trySet(request);
+    int channel_id = request->hasParam("channel_id") ? request->getParam("channel_id")->value().toInt() : 0;
+
+    // handle persistent per-channel parameters
+    
+    for(Param* p = channels[channel_id].getParams(); p ; p = p->next())
+      if (request->hasParam(p->desc->name) ) {
+        p->set(request->getParam(p->desc->name)->value().toInt());
+        char prefs_name[32];
+        snprintf(prefs_name, sizeof(prefs_name), "%s_%d", p->desc->name, channel_id);
+        prefs.putInt(prefs_name, request->getParam(p->desc->name)->value().toInt());
+      }
+      
+    // check for global parameters
+    for(char* param : global_params)
+      if (request->hasParam(param) )
+        prefs.putString(param, request->getParam(param)->value());
 
     // handle instantaneous commands
     if (request->hasParam("target") )
-      manual_target = request->getParam("target")->value().toInt();
+      channels[channel_id].manual_target = request->getParam("target")->value().toInt();
     //if (request->hasParam("set_position") )
     //  stepper->setCurrentPosition(request->getParam("set_position")->value().toInt());
     if (request->hasParam("enable") ) {
-      enabled = request->getParam("enable")->value().toInt();
-      if(have_alarm) {
-        channels[0].init_motor();
-        have_alarm = 0;
+      channels[channel_id].enabled = request->getParam("enable")->value().toInt();
+      if(channels[channel_id].have_alarm) {
+        channels[channel_id].init();
+        channels[channel_id].have_alarm = 0;
       }
     }
     if (request->hasParam("home") )
       homing = request->getParam("home")->value().toInt();
     if (request->hasParam("reset") )
       ESP.restart();
-
-    channels[0].cybergear.set_limit_current(enabled * prefs.getInt("accel",10000)/1000.f); 
   });
 
   // http "status" API to query current status
   httpServer->on("/status", HTTP_GET, [](AsyncWebServerRequest *request) {
     AsyncResponseStream *response = request->beginResponseStream("text/html");
-    
-     XiaomiCyberGearStatus status = channels[0].cybergear.get_status();
-  // Serial.printf("POS:%f V:%f T:%f temp:%d\n", cybergear_status.position, cybergear_status.speed, cybergear_status.torque, cybergear_status.temperature);
-    
-    response->printf("%s %d\n","target",target);
+
+    int channel_id = request->hasParam("channel_id") ? request->getParam("channel_id")->value().toInt() : 0;    
+    XiaomiCyberGearStatus status = channels[channel_id].cybergear.get_status();
+    response->printf("%s %d\n","target",channels[channel_id].target);
     response->printf("%s %d\n","position",(int32_t)round(status.position*1000.f));
     response->printf("%s %d\n","torque",(int32_t)round(status.torque*1000.f));
     response->printf("%s %d\n","temperature",(int32_t)status.temperature);
-    response->printf("%s %d\n","enable",enabled);
-    response->printf("%s %d\n","alarm",have_alarm);
+    response->printf("%s %d\n","enable",channels[channel_id].enabled);
+    response->printf("%s %d\n","alarm",channels[channel_id].have_alarm);
     request->send(response);
   });
 
   // http "config" API to query persistent configuration
   httpServer->on("/config", HTTP_GET, [](AsyncWebServerRequest *request) {
     AsyncResponseStream *response = request->beginResponseStream("text/html");
-    for(Param& p : params)
-      if(p.type == Param::INT)
-        response->printf("%s %d\n",p.name,prefs.getInt(p.name));
-      else if(p.type == Param::STRING)
-        response->printf("%s %s\n",p.name, prefs.getString(p.name).c_str());
+    for(char* param : global_params)
+      response->printf("%s %s\n",param, prefs.getString(param,"").c_str());
     request->send(response);
   });
 
@@ -290,10 +310,12 @@ void setup()
 
   // start ArtnetNode
   artnetnode.setArtDmxCallback([](uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data){
-    if(!dmx_enabled) return;
-    uint8_t dmxChannel = prefs.getInt("channel");
-    if(dmxChannel + 1 >= length) return;
-    artnet_target = prefs.getInt("scale") * ( ((uint32_t)data[0+dmxChannel]) + (((uint32_t)data[1+dmxChannel])<<8) );
+    for(uint8_t channel_id = 0; channel_id<max_channels; channel_id++) {
+      Channel& c = channels[channel_id];
+      if(c.channel < length)
+        c.artnet_target = c.scale * ( ((uint32_t)data[0+c.channel]) + (((uint32_t)data[1+c.channel])<<8) );
+    }
+
   });
   artnetnode.begin();
   Serial.println("ArtNet node started.");
@@ -302,12 +324,7 @@ void setup()
   digitalWrite(statusLedPin,HIGH);
 }
 
-
 uint32_t last_time;
-uint32_t last_status;
-float osc_phase = 0;
-int32_t random_countdown = 0;
-int32_t random_target = 0;
 
 void loop() 
 {
@@ -322,29 +339,8 @@ void loop()
   last_time = time;
   //Serial.println(dt);
 
-  // compute and set motion target
-  // set manual target (offset)
-  target = manual_target;
-
-  // add artnet commanded target
-  target += artnet_target;
- 
-  // add oscillatory movement 
-  osc_phase += prefs.getInt("osc_f") * dt * 2.f * 3.14159f / 1000.f / 1000.f;
-  osc_phase = fmod(osc_phase, ((float)PI * 2.f));
-  target += floor( prefs.getInt("osc_a") * sin(osc_phase) );
-  
-  // add random movement
-  random_countdown -= dt;
-  if(random_countdown < 0) {
-    random_target    = random(prefs.getInt("random_a"));
-    random_countdown = prefs.getInt("random_d") + random(prefs.getInt("random_rd"));
-  }
-  target += random_target;
-
-  // execute move
-  channels[0].update();
-  channels[0].cybergear.set_position_ref(target/1000.f);
+  for(Channel& c : channels)
+    c.update(dt);
  
   // check motor state
   // also updates motor status data that can be relayed to wifi clients then.
